a1ea7859f8d0e9e62f0f58d1c5504be6a365354b
Sherine Awad <sherinem@gl3165.arc-ts.umich.edu>
2025-11-25 15:44:01 -0500

    fix gradient in plotPeaks- update corresponding heatmap

--- README.md
+++ README.md
@@ -163,7 +163,8 @@ myObject.atac.markers <- FindAllMarkers(
 
 [Merged Ranked DiffPeaks](https://docs.google.com/spreadsheets/d/1WrM1nD6XbIRr8Eo9RFqT8Ba4bGvVSVx9u4QsQ7ihyxY/edit?usp=sharing)
 
-![](figures/TopPeaksNearbyheatmap.png?v=2)
+![](figures/TopPeaksNearbyheatmap.png?v=3)
+
 
 #  Subset MG/MGPC 
 
--- plotPeaks.R
+++ plotPeaks.R
@@ -4,6 +4,13 @@ library(Signac)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(scales)
+
+# ----------------------------
+
+# Arguments
+
+# ----------------------------
 
 parser <- ArgumentParser()
 parser$add_argument("--rds", required=TRUE)
@@ -12,42 +19,48 @@ parser$add_argument("-o", required=TRUE)
 parser$add_argument("-g", required=TRUE, help="Text file with a list of gene names to include")
 args <- parser$parse_args()
 
-obj <- readRDS(args$rds)
+# ----------------------------
 
-if (is.null(obj@misc$diff_peaks)) stop("diff_peaks not found.")
-if (is.null(obj@misc$peak_annotations)) stop("peak_annotations not found.")
+# Load Seurat object and data
+
+# ----------------------------
+
+obj <- readRDS(args$rds)
+stopifnot(!is.null(obj@misc$diff_peaks), !is.null(obj@misc$peak_annotations))
 
 diff <- obj@misc$diff_peaks
 ann  <- obj@misc$peak_annotations
-
 diff <- diff %>% rename(peak_id = gene)
 
-if (!"gene_name" %in% colnames(ann)) stop("peak_annotations must contain gene_name.")
-if (!"query_region" %in% colnames(ann)) stop("peak_annotations must contain query_region.")
-if (!"avg_log2FC" %in% colnames(diff)) stop("diff_peaks must contain avg_log2FC")
-if (!"cluster" %in% colnames(diff)) stop("diff_peaks must contain cluster")
+stopifnot(all(c("gene_name","query_region") %in% colnames(ann)))
+stopifnot(all(c("avg_log2FC","cluster") %in% colnames(diff)))
 
 cell_order <- c("MG","MGPC","BC","AC","Rod","Cones")
 diff <- diff %>% filter(cluster %in% cell_order)
 
-merged <- diff %>%
-left_join(ann %>% select(query_region, gene_name),
-by = c("peak_id" = "query_region"))
+# ----------------------------
 
-# --------------------------------------------------------------------
+# Merge annotations (allow many-to-many)
+
+# ----------------------------
+
+merged <- diff %>%
+left_join(
+ann %>% select(query_region, gene_name),
+by = c("peak_id" = "query_region"),
+relationship = "many-to-many"
+)
+
+# ----------------------------
 
 # Build matrix: rows = peaks, columns = clusters
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 mat_all <- merged %>%
 select(peak_id, cluster, avg_log2FC, gene_name) %>%
 distinct() %>%
-pivot_wider(
-names_from = cluster,
-values_from = avg_log2FC,
-values_fill = 0
-)
+pivot_wider(names_from = cluster, values_from = avg_log2FC, values_fill = 0)
 
 mat_all <- mat_all[, c("peak_id","gene_name", cell_order)]
 peak_ids <- mat_all$peak_id
@@ -56,49 +69,47 @@ gene_labels <- mat_all$gene_name
 mat_dense <- as.matrix(mat_all[, cell_order])
 rownames(mat_dense) <- peak_ids
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 # Compute z-scores per peak
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 mat_z <- t(scale(t(mat_dense), center = TRUE, scale = TRUE))
 mat_z[is.na(mat_z)] <- 0
 rownames(mat_z) <- peak_ids
 colnames(mat_z) <- cell_order
 
-# --------------------------------------------------------------------
+# ----------------------------
 
-# Select top N peaks per cell type
+# Select top N peaks per cluster (both up and down)
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 top_peak_ids <- unique(unlist(lapply(cell_order, function(cl) {
 cl_scores <- mat_z[, cl]
-top_idx <- order(abs(cl_scores), decreasing = TRUE)[1:min(args$n, length(cl_scores))]
-rownames(mat_z)[top_idx]
+top_idx <- order(cl_scores, decreasing = TRUE)[1:min(args$n, length(cl_scores))]
+bottom_idx <- order(cl_scores, decreasing = FALSE)[1:min(args$n, length(cl_scores))]
+rownames(mat_z)[c(top_idx, bottom_idx)]
 })))
 
 mat_top <- mat_z[top_peak_ids, , drop=FALSE]
 gene_labels_top <- gene_labels[match(rownames(mat_top), peak_ids)]
 
-# --------------------------------------------------------------------
+# ----------------------------
 
-# Load gene list
+# Load gene list and label
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 gene_list <- read.table(args$g, stringsAsFactors = FALSE)[,1]
-
-# Only show genes in the list
-
 y_labels <- ifelse(gene_labels_top %in% gene_list, gene_labels_top, "")
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 # Convert for ggplot
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 gene_scores <- mat_top %>%
 as.data.frame() %>%
@@ -117,11 +128,11 @@ gene_scores$row_id,
 levels = unique(gene_scores$row_id[order(gene_scores$maxpos)])
 )
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 # Colors
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 custom_palette <- c(
 "#2166AC",
@@ -132,20 +143,19 @@ custom_palette <- c(
 "#EF8A62",
 "#B2182B"
 )
-value_breaks <- scales::rescale(c(-2, -1, -0.1, 0, 0.3, 1, 2))
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 # Plot heatmap
 
-# --------------------------------------------------------------------
+# ----------------------------
 
 png(args$o, width=1600, height=2000, res=150)
 ggplot(gene_scores, aes(x=cluster, y=row_id, fill=score)) +
 geom_tile() +
 scale_fill_gradientn(
 colors = custom_palette,
-values = value_breaks,
+limits = c(min(gene_scores$score), max(gene_scores$score)),
 name = "Z-score"
 ) +
 scale_y_discrete(labels = y_labels) +
@@ -157,3 +167,4 @@ axis.text.y = element_text(size=10),
 panel.grid = element_blank()
 )
 dev.off()
+
